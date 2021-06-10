5c4220d7a10b752d8445c8115b6286a49164b84e
Michael Merchan <72261574+MauricioGH92@users.noreply.github.com>
2021-06-10 14:59:55 +0000

    decalracion de constantes.

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hola mundo")
+  //declaracion de constantes
+  const pi float64 = 3.14
+  const pi2 = 3.1314
+  fmt.Println("pi:",pi)
+  fmt.Println("pi2:",pi2)
+
 }
