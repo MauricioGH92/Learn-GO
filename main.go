13aad001964d39e2da4ca6fe3453b785c2060cd4
Michael Merchan <72261574+MauricioGH92@users.noreply.github.com>
2021-06-11 15:40:23 +0000

    String personalizados.

--- main.go
+++ main.go
@@ -9,34 +9,10 @@ type pc struct {
 	disk  int
 	brand string
 }
-
-func (myPc pc) ping() {
-	fmt.Println(myPc.brand, "Pong")
+func (myPc pc) String() string{
+  return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPc.ram,myPc.disk, myPc.brand )
 }
-
-func (myPc *pc) duplicateRAM() {
-	myPc.ram = myPc.ram * 2
-}
-
-func main() {
-  
-  a := 50
-  b := &a
-
-  fmt.Println(b)
-  fmt.Println(*b)
-
- 
-
+func main() { 
   myPc := pc{ram: 16, disk: 200, brand: "msi"}
-	myPc.ping()
-
 	fmt.Println(myPc)
-
-	myPc.duplicateRAM()
-	fmt.Println(myPc)
-	
-	myPc.duplicateRAM()
-	fmt.Println(myPc)
-
 }
