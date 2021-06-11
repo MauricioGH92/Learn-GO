b5abbe458b99a80b2302d8fbf4d531490a256cd5
Michael Merchan <72261574+MauricioGH92@users.noreply.github.com>
2021-06-11 13:59:02 +0000

    Desafio identificar palindromo.

--- main.go
+++ main.go
@@ -1,33 +1,28 @@
 package main
 
-	
 import (
     "fmt"
+    "strings"
+    "reflect"
 )
 
+func reverseString(input []string) []string {
+    if len(input) == 0 {
+        return input
+    }
+    return append(reverseString(input[1:]), input[0]) 
+}
+
+func isPalindromo(word string) bool{
+  splitWord := strings.Split(strings.ToUpper(word), "")
+  return reflect.DeepEqual(splitWord, reverseString(splitWord))
+}
 
 func main() {
-  //Array inmutables
-  var array [4] int
-  array[0] = 1
-  array[1] = 2
-  fmt.Println(array,len(array),cap(array))
-
-  //slice mutables
-  var slice []int
-  slice = append(slice, 2)
-  slice = append(slice, 2)
-  slice = append(slice, 2)
-  fmt.Println(slice,len(slice),cap(slice))
-
-  var slice2 = []int{1,2,3}
-  fmt.Println(slice2,len(slice2),cap(slice2))
-
-  slice2 =append(slice2,slice... )
-
-  fmt.Println(array[1:])
-  fmt.Println(slice2)
-
-
-
+  word :="Ama"
+  if(isPalindromo(word)){
+    fmt.Println("Es palindromo")
+  }else{
+    fmt.Println("No es palindromo")
+  }
 }
