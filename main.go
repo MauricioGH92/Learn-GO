ddd43e26ffb8a1bf9c7b2d102da31532b3b26607
Michael Merchan <72261574+MauricioGH92@users.noreply.github.com>
2021-06-11 14:12:53 +0000

    estructura de mapas

--- main.go
+++ main.go
@@ -2,27 +2,25 @@ package main
 
 import (
     "fmt"
-    "strings"
-    "reflect"
 )
 
-func reverseString(input []string) []string {
-    if len(input) == 0 {
-        return input
-    }
-    return append(reverseString(input[1:]), input[0]) 
-}
-
-func isPalindromo(word string) bool{
-  splitWord := strings.Split(strings.ToUpper(word), "")
-  return reflect.DeepEqual(splitWord, reverseString(splitWord))
-}
-
 func main() {
-  word :="Ama"
-  if(isPalindromo(word)){
-    fmt.Println("Es palindromo")
-  }else{
-    fmt.Println("No es palindromo")
+  //mapas
+  m := make(map[string]int)
+  m["jose"] =14
+  m["Pepito"] =20
+  m["Pepito1"] =21
+  m["Pepito2"] =22
+
+
+  for i,v :=range m {
+    fmt.Println(i,v)
   }
+
+  value, existe := m["jose"]
+  fmt.Println(value,existe)
+  value, existe = m["josep"]
+  fmt.Println(value,existe)
+
+
 }
