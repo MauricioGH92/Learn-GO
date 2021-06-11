4d33af99a0bda1aab77d52fb535a09637162e8a4
Michael Merchan <72261574+MauricioGH92@users.noreply.github.com>
2021-06-11 14:17:12 +0000

    Struct

--- main.go
+++ main.go
@@ -4,23 +4,17 @@ import (
     "fmt"
 )
 
+type car struct{
+  brand string
+  year int
+}
+
 func main() {
-  //mapas
-  m := make(map[string]int)
-  m["jose"] =14
-  m["Pepito"] =20
-  m["Pepito1"] =21
-  m["Pepito2"] =22
+  myCar := car{brand:"Ford", year: 2020}
+  fmt.Println(myCar)
 
-
-  for i,v :=range m {
-    fmt.Println(i,v)
-  }
-
-  value, existe := m["jose"]
-  fmt.Println(value,existe)
-  value, existe = m["josep"]
-  fmt.Println(value,existe)
-
+  var otherCar car
+  otherCar.brand = "Ferrari"
+  fmt.Println(otherCar)
 
 }
