17977ecbff36065a39208dec740dbea0975f7f8f
Michael Merchan <72261574+MauricioGH92@users.noreply.github.com>
2021-06-11 12:28:39 +0000

    Array y slice

--- main.go
+++ main.go
@@ -7,23 +7,27 @@ import (
 
 
 func main() {
- 
-  //swith
-  modulo := 4%2
-  switch modulo{
-    case 0:
-      fmt.Println("Es par")
-    default:
-      fmt.Println("Es impar")
-  }
+  //Array inmutables
+  var array [4] int
+  array[0] = 1
+  array[1] = 2
+  fmt.Println(array,len(array),cap(array))
 
-  value:=200
-  switch{
-    case value>100:
-      fmt.Println("Es mayor a 100")
-    case value<0:
-      fmt.Println("Es menor a 0")
-    default:
-      fmt.Println("Nocondicion")
-  }
+  //slice mutables
+  var slice []int
+  slice = append(slice, 2)
+  slice = append(slice, 2)
+  slice = append(slice, 2)
+  fmt.Println(slice,len(slice),cap(slice))
+
+  var slice2 = []int{1,2,3}
+  fmt.Println(slice2,len(slice2),cap(slice2))
+
+  slice2 =append(slice2,slice... )
+
+  fmt.Println(array[1:])
+  fmt.Println(slice2)
+
+
+
 }
