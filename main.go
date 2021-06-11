13bb0440305a97052e077ef31e046f027c04fd9d
Michael Merchan <72261574+MauricioGH92@users.noreply.github.com>
2021-06-11 15:54:52 +0000

    interfaces y lista de interfaces

--- main.go
+++ main.go
@@ -2,17 +2,63 @@ package main
 
 import (
     "fmt"
+    "math"
+	  "reflect"
 )
 
-type pc struct {
-	ram   int
-	disk  int
-	brand string
+type figure2D interface {
+	getArea() float64
 }
-func (myPc pc) String() string{
-  return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPc.ram,myPc.disk, myPc.brand )
+
+type square struct {
+	base float64
 }
+
+type rectangle struct {
+	high  float64
+	width float64
+}
+type trapezoid struct {
+	baseA float64
+	baseB float64
+	high  float64
+}
+
+type circle struct {
+	radio float64
+}
+func (s square) getArea() float64 {
+	return s.base * s.base
+}
+
+func (r rectangle) getArea() float64 {
+	return r.high * r.width
+}
+
+func (t trapezoid) getArea() float64 {
+	return ((t.baseA + t.baseB) / 2) * t.high
+}
+
+func (c circle) getArea() float64 {
+	return math.Pi * math.Pow(c.radio, 2)
+}
+
+func calculateArea(f figure2D) {
+	fmt.Printf("Area of %s: %.2f\n", reflect.TypeOf(f).Name(), f.getArea())
+}
+
+
 func main() { 
-  myPc := pc{ram: 16, disk: 200, brand: "msi"}
-	fmt.Println(myPc)
+  mySquare := square{base: 5}
+  myRectangle := rectangle{width: 4, high: 5}
+	myTrapezoid := trapezoid{baseA: 18, baseB: 10, high: 5}
+	myCircle := circle{radio: 4}
+  calculateArea(mySquare)
+	calculateArea(myRectangle)
+	calculateArea(myTrapezoid)
+	calculateArea(myCircle)
+
+  myInterfaceList := []interface{}{"string",12,4.9}
+  fmt.Println(myInterfaceList)
+
 }
